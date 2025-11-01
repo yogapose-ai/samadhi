57eea131f27da61dd02ae4a2273b506b00622030
정서영 <diyung530@gmail.com>
2025-11-01 22:35:44 +0900

    fe check

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_API_URL + "/:path*",
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
